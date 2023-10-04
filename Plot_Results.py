5ca9719312e46a68cb4e05d00619ea9afcdec598
CrimsonSurfer <awalke17@uccs.edu>
2023-10-03 23:31:02 -0600

    2/3 plots complete

--- Plot_Results.py
+++ Plot_Results.py
@@ -30,27 +30,31 @@ def plot_runtimes(E, G):
     # save as Plot.png
     export_png(p, filename="Runtimes.png")
 
-def plot_Items(Objects):
-    Weights = []
-    Values = []
-    for weight, values in Objects.values():
-        Weights.append(weight)
-        Values.append(values)
-    
-    # create scatter plot layout
-    p = figure(width=600, height=400, title="Item spread")
-    p.add_layout(Title(text="Weights", align="center"), "below")
-    p.add_layout(Title(text="Values", align="center"), "left")
+def plot_Items(Total_Weights_E, Total_Weight_G):
+    E = list(Total_Weights_E)
+    G = list(Total_Weight_G)
+
+
+    Number_of_items = list(range(3, 16))
+    p = figure(width=600, height=400, title="Average Weights")
+    p.add_layout(Title(text="Number of Items", align="center"), "below")
+    p.add_layout(Title(text="Total Weight", align="center"), "left")
 
     # add data to scatter plot
-    p.circle(Weights, Values, color="Blue")
+    p.circle(Number_of_items, E, color="Blue", legend_label="Exhaustive")
+    p.square(Number_of_items, G, color="Green", legend_label="Greedy")
+
+    # add legend
+    p.legend.location = "top_right"
+    p.legend.title = "Search Algorithm"
+    p.add_layout(p.legend[0], 'right')
 
     # remove toolbar on graph
     p.toolbar.logo = None
     p.toolbar_location = None
 
-    export_png(p, filename="Items.png")
-
+    # save as Plot.png
+    export_png(p, filename="Total_Weights.png")
     
 
 
